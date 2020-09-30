b515153b285712de6d5ead2de1921f40833df068
JMartiniano <jose.martiniano01@gmail.com>
2020-09-29 22:10:41 -0300

    Resolvendo questões

--- Lista3_Operadores_Condicionais/q6.sh
+++ Lista3_Operadores_Condicionais/q6.sh
@@ -2,6 +2,6 @@
 
 read -p "Texto proibido: " busca
 
-cat $1 | grep ${busca} &> /dev/null && mv $1 /tmp
-cat $2 | grep ${busca} &> /dev/null && mv $2 /tmp
-cat $3 | grep ${busca} &> /dev/null && mv $3 /tmp
+cat $1 | grep ${busca} &> /dev/null && mv $1 /tmp && echo "$1"
+cat $2 | grep ${busca} &> /dev/null && mv $2 /tmp && echo "$2"
+cat $3 | grep ${busca} &> /dev/null && mv $3 /tmp && echo "$3"
