390aea07e4f26a0951dbbb78dffa8598a505dc14
JMartiniano <jose.martiniano01@gmail.com>
2020-12-02 23:08:38 -0300

    Finalizado

--- Lista12_Arrays/q2.sh
+++ Lista12_Arrays/q2.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-IFS=" "
-
 declare -A AB
 
-cont=0
 while read l; do
-	read -a a <<< "${l}"
-	nomes=( ${nomes[*]} ${a[0]} )
-	numeros=( ${numeros[*]} ${a[1]} )
+
+	key=$( echo ${l} | awk '{print $1}')
+	valor=$( echo ${l} | awk '{print $2}' )
+
+	AB["${key}"]=$(( ${AB[${key}]} + ${valor} ))
 	
-	AB["${nomes[${cont}]}"]="${numeros[$cont]}"
-	
-	cont=$(( ${cont} + 1 ))
 done < $1
 
-echo "Nomes: ${nomes[*]}"
-echo "Números: ${numeros[*]}"
-
-echo "Criando dicionários"
-
-echo ${!AB[*]} ${AB[*]}
+echo -e "${!AB[*]}\n${AB[*]}"
